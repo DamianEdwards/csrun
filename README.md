5fd98d3c21500b842beff9251a1f6adbc001540c
Damian Edwards <damian@damianedwards.com>
2025-03-13 16:15:06 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,50 @@
+# csrun
+
+A tool that makes it easy to run standalone C# files.
+
+> Currently requires [.NET 10 SDK daily build](https://github.com/dotnet/sdk/blob/main/documentation/package-table.md), version *10.0.100-preview.3.25163.13* or later
+
+## Installation
+
+```bash
+dotnet tool install -g dotnet-cs
+```
+
+## Usage
+
+```bash
+cs <TARGETAPPFILE> [<APPARGS>]
+```
+
+### Arguments
+
+Name  | Description
+------|------------------------------------------------
+&lt;TARGETAPPFILE&gt; | The path to the C# file to run. This can be a relative or absolute path, or a URI to a remote file.
+&lt;APPARGS&gt; | Any arguments that should be passed to the C# app.
+
+### Examples
+
+Run a C# file named `hello.cs` in the current directory:
+
+```bash
+~/apps
+$ cs hello.cs
+Hello, world!
+```
+
+Run a C# file named `hello.cs` in a sub-directory and pass an argument to it:
+
+```bash
+~/apps
+$ cs ./utils/hello.cs David
+Hello, David!
+```
+
+Run a C# file from `https://github.com/DamianEdwards/csrun/tree/main/samples/hello.cs` and pass an argument to it:
+
+```bash
+~/apps
+$ cs https://github.com/DamianEdwards/csrun/tree/main/samples/hello.cs Stephen
+Hello, Stephen!
+```
---
+++ src/dotnet-cs/Program.cs
@@ -0,0 +1,199 @@
+﻿using System.CommandLine;
+using System.CommandLine.Invocation;
+using System.Diagnostics;
+using System.Reflection;
+
+var targetArgument = new CliArgument<string?>("TARGETAPPFILE")
+{
+    Description = "The path to the C# file to run. This can be a relative or absolute path, or a URI to a remote file.",
+    Arity = ArgumentArity.ExactlyOne
+};
+
+var appArgsArgument = new CliArgument<string[]?>("APPARGS")
+{
+    Description = "The arguments to pass to the C# file.",
+    Arity = ArgumentArity.ZeroOrMore
+};
+
+var rootCommand = new CliRootCommand("Runs standalone C# files.")
+{
+    targetArgument,
+    appArgsArgument
+};
+rootCommand.SetAction(RunCommand);
+
+VersionOptionAction.Apply(rootCommand);
+
+var result = rootCommand.Parse(args);
+var exitCode = await result.InvokeAsync();
+
+return exitCode;
+
+async Task<int> RunCommand(ParseResult parseResult, CancellationToken cancellationToken)
+{
+    var targetValue = parseResult.GetValue(targetArgument);
+
+    if (string.IsNullOrEmpty(targetValue))
+    {
+        WriteError("No target file specified.");
+        return 1;
+    }
+
+    string? targetFilePath = null;
+
+    if ((targetValue.StartsWith("https://", StringComparison.OrdinalIgnoreCase) || targetValue.StartsWith("http://", StringComparison.OrdinalIgnoreCase))
+        && Uri.TryCreate(targetValue, UriKind.Absolute, out var uri))
+    {
+        // If it's a URI, download the file to a temporary location
+        Write($"Downloading file from {uri}... ", ConsoleColor.DarkGray);
+        var tempFilePath = Path.GetTempFileName();
+        using var client = new HttpClient();
+        var response = await client.GetAsync(uri, cancellationToken);
+
+        if (!response.IsSuccessStatusCode)
+        {
+            Write("failed", ConsoleColor.DarkGray);
+            WriteError($"Failed to download file from {uri}. Status code: {response.StatusCode}");
+            return 1;
+        }
+
+        WriteLine("done", ConsoleColor.DarkGray);
+
+        await using (var fileStream = File.Create(tempFilePath))
+        {
+            await response.Content.CopyToAsync(fileStream, cancellationToken);
+        }
+
+        // Change the file to a .cs extension
+        var csFilePath = Path.Join(Path.GetDirectoryName(tempFilePath), Path.GetFileNameWithoutExtension(tempFilePath) + ".cs");
+        File.Move(tempFilePath, csFilePath, true);
+
+        targetFilePath = csFilePath;
+    }
+    else if (File.Exists(targetValue))
+    {
+        // If it's a local file, use the provided path
+        targetFilePath = Path.GetFullPath(targetValue);
+    }
+    else
+    {
+        WriteError($"File not found: {targetValue}");
+        return 1;
+    }
+
+    var appArgs = parseResult.GetValue(appArgsArgument);
+
+    await DotnetCli.Run(targetFilePath, appArgs, cancellationToken);
+
+    return 0;
+}
+
+static void WriteError(string message) => WriteLine(message, ConsoleColor.Red);
+
+static void WriteLine(string? message = null, ConsoleColor? color = default)
+{
+    if (!string.IsNullOrEmpty(message))
+    {
+        Write(message, color);
+    }
+    Console.WriteLine();
+}
+
+static void Write(string? message = null, ConsoleColor? color = default)
+{
+    if (color is not null)
+    {
+        var originalColor = Console.ForegroundColor;
+        Console.ForegroundColor = color.Value;
+        Console.Write(message);
+        Console.ForegroundColor = originalColor;
+    }
+    else
+    {
+        Console.Write(message);
+    }
+}
+
+static class DotnetCli
+{
+    private static readonly string[] RunArgs = ["run"];
+
+    public async static Task<int> Run(string filePath, string[]? args, CancellationToken cancellationToken)
+    {
+        var arguments = RunArgs.Concat([filePath]);
+        if (args is { Length: > 0 })
+        {
+            arguments = arguments.Concat(args);
+        }
+        var startInfo = GetProcessStartInfo(arguments);
+        startInfo.CreateNoWindow = false;
+        startInfo.RedirectStandardOutput = false;
+        var process = Start(startInfo);
+
+        await process.WaitForExitAsync(cancellationToken);
+
+        return process.ExitCode;
+    }
+
+    private static Process Start(IEnumerable<string> arguments) => Start(GetProcessStartInfo(arguments));
+
+    private static Process Start(ProcessStartInfo startInfo)
+    {
+        var process = new Process { StartInfo = startInfo };
+
+        return process.Start() ? process : throw new Exception("Failed to start process");
+    }
+
+    private static ProcessStartInfo GetProcessStartInfo(IEnumerable<string> arguments)
+    {
+        var info = new ProcessStartInfo
+        {
+            FileName = "dotnet",
+            RedirectStandardOutput = false,
+            RedirectStandardError = false,
+            UseShellExecute = false,
+            CreateNoWindow = true
+        };
+
+        foreach (var arg in arguments)
+        {
+            info.ArgumentList.Add(arg);
+        }
+
+        return info;
+    }
+}
+
+internal sealed class VersionOptionAction : SynchronousCliAction
+{
+    public static void Apply(CliRootCommand command)
+    {
+        var versionOption = command.Options.FirstOrDefault(o => o.Name == "--version");
+        if (versionOption is not null)
+        {
+            versionOption.Action = new VersionOptionAction();
+        }
+    }
+
+    public override int Invoke(ParseResult parseResult)
+    {
+        var currentVersion = GetCurrentVersion();
+        parseResult.Configuration.Output.WriteLine(currentVersion ?? "<unknown>");
+
+        return 0;
+    }
+
+    public static string? GetCurrentVersion()
+    {
+        var assembly = typeof(Program).Assembly;
+        var informationalVersion = assembly.GetCustomAttribute<AssemblyInformationalVersionAttribute>()?.InformationalVersion;
+        if (!string.IsNullOrEmpty(informationalVersion))
+        {
+            // Remove the commit hash from the version string
+            var versionParts = informationalVersion.Split('+');
+            return versionParts[0];
+        }
+
+        return assembly.GetName().Version?.ToString();
+    }
+}
